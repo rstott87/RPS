c21a05a52f9c91546eafa823cdd1def28ae2495d
rstott87 <rstott87@yahoo.com>
2021-09-18 19:25:55 -0400

    finished rock paper scissors

--- main.js
+++ main.js
@@ -4,15 +4,63 @@ let x = Math.floor(Math.random ()*3);
 
 if (x == 0) 
 {
-    x = "Rock";
+    x = "rock";
 }
 else if (x == 1) 
 {
-    x = "Paper";
+    x = "paper";
 }
 else    
 {
-    x = "Scissors";
+    x = "scissors";
 }
 return x;
 }
+
+let computerSelection = computerPlay();
+let playerSelection = prompt ("Pick rock, paper, or scissors: ".toLowerCase());
+
+let game = (computerSelection, playerSelection) => 
+{
+    let c = computerSelection;
+    let p = playerSelection;
+   
+    let rock = "rock";
+    let paper = "paper";
+    let scissors = "scissors";
+
+    let winner = "You win!"
+    let loser = "You lose!"
+
+    if (c == "rock" && p == "scissors")
+        { 
+        return (loser);
+        }
+    else if (c == "rock" && p == "paper")
+        { 
+        return (winner);
+        }
+     else if (c == "paper" && p == "rock")
+        { 
+        return (loser);
+        }
+    else if (c == "paper" && p == "scissors")
+        { 
+        return (winner);
+        }
+    else if (c == "scissors" && p == "paper")
+        { 
+        return (loser);
+        }
+    else if (c == "scissors" && p == "rock")
+        { 
+        return (winner);
+        }
+    else {
+        return "It's a tie!";
+    }    
+    }
+
+
+    
+    console.log (`${game(computerSelection, playerSelection)} The computer chose ${computerSelection} and you chose ${playerSelection} .`);
